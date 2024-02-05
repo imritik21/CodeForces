ce6adab9a4baeaf912c3d500621a9372e7e32478
imritik21 <141487541+imritik21@users.noreply.github.com>
2024-02-05 13:21:26 +0530

    First Commit

--- CSES/WeirdAlgo.cpp
+++ CSES/WeirdAlgo.cpp
@@ -4,5 +4,16 @@ using namespace std;
 #define endl '\n'
 signed main(){
     int num;
-    
+    cin>>num;
+    while(num>1){
+        if(num%2!=0){
+            cout<<num<<" ";
+            num=num*3+1;
+        }
+        else{
+            cout<<num<<" ";
+            num=num/2;
+        }
+    }
+    cout<<1;
 }
